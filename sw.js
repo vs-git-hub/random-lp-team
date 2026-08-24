f97352fdc405a7554e75914fbd45ecbaf1a83ceb
vs-git-hub <valerkasav@gmail.com>
2026-08-24 17:13:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,82 @@
-const CACHE_NAME = "random-lp-team-v1";
-const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./sw.js",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./apple-touch-icon.png",
-  "./favicon.png"
+const CACHE_NAME = 'random-lp-v4';
+
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png',
+  './favicon.png'
 ];
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(FILES);
-    })
-  );
+self.addEventListener('install', event => {
   self.skipWaiting();
-});
 
-self.addEventListener("activate", function(event) {
   event.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.map(function(key) {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      );
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(STATIC_ASSETS);
     })
   );
-  self.clients.claim();
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => {
+        return Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request, { cache: 'no-store' })
+        .then(response => {
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put('./index.html', copy);
+          });
+
+          return response;
+        })
+        .catch(() => {
+          return caches.match('./index.html');
+        })
+    );
+
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    caches.match(request).then(cached => {
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(request).then(response => {
+        if (
+          response.ok &&
+          new URL(request.url).origin === location.origin
+        ) {
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(request, copy);
+          });
+        }
+
+        return response;
+      });
     })
   );
 });
